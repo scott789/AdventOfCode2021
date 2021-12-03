20523049073cf3b0f0791d92763beeac291cc46b
scott <18712626+scott789@users.noreply.github.com>
2021-12-03 09:31:42 -0500

    Complete answer for day 3 part 2

--- day_3/day3_2.py
+++ day_3/day3_2.py
@@ -18,7 +18,10 @@ def calculate(file_name):
         for line in file:
             rows.append(line.rstrip())
 
+    rows_copy = rows.copy()
+
     oxygen_generator_rating_bits = ''
+    co2_scrubber_rating_bits = ''
 
     row_length = len(rows[0])
 
@@ -34,14 +37,34 @@ def calculate(file_name):
                 else:
                     oxygen_generator_rating_bits = e
 
+    for col_ix in range(0, row_length):
+        count_0 = number_of_bits_at_index(rows_copy, '0', col_ix)
+        count_1 = number_of_bits_at_index(rows_copy, '1', col_ix)
+        smaller_number = 1 if count_1 < count_0 else 0
+
+        for i, e in reversed(list(enumerate(rows_copy))):
+            if int(e[col_ix]) != smaller_number:
+                if len(rows_copy) > 1:
+                    rows_copy.pop(i)
+                else:
+                    co2_scrubber_rating_bits = e
+
     print(f'rows: {rows}')
+    print(f'rows_copy: {rows_copy}')
 
     if oxygen_generator_rating_bits == '':
         oxygen_generator_rating_bits = rows[0]
 
+    if co2_scrubber_rating_bits == '':
+        co2_scrubber_rating_bits = rows_copy[0]
+
     oxygen_generator_rating = int(oxygen_generator_rating_bits, 2)
+    co2_scrubber_rating = int(co2_scrubber_rating_bits, 2)
+    answer = oxygen_generator_rating * co2_scrubber_rating
 
     print(f'oxygen_generator_rating: {oxygen_generator_rating}')
+    print(f'co2_scrubber_rating: {co2_scrubber_rating}')
+    print(f'answer: {answer}')
 
 
 def main():
